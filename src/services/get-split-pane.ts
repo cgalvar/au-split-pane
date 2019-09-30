6300c361a08d78c0407a4c654fe13363fb057864
cgalvar <julio_aguila@hotmail.es>
2019-09-30 17:10:45 -0500

    Se agrega el autoinject que falto y que da error

--- src/services/get-split-pane.ts
+++ src/services/get-split-pane.ts
@@ -1,7 +1,9 @@
 import { GetViewmodel } from "./get-viewmodel";
 import { EventAggregator } from "aurelia-event-aggregator";
 import { SplitPane } from "../elements/split-pane";
+import { autoinject } from "aurelia-framework";
 
+@autoinject
 export class GetSplitPane extends GetViewmodel{
     constructor(private eventAgregator:EventAggregator) {
         super(eventAgregator, 'split-pane');
